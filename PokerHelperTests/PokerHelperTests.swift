6cb2d40fd8905c138ad2276d50d8f25ff48f3796
rodolico26 <rodolico26@icloud.com>
2020-04-21 15:58:27 +0200

    Initial Commit

---
+++ PokerHelperTests/PokerHelperTests.swift
@@ -0,0 +1,34 @@
+//
+//  PokerHelperTests.swift
+//  PokerHelperTests
+//
+//  Created by Rodolico26 on 21/04/2020.
+//  Copyright © 2020 rechargingcable. All rights reserved.
+//
+
+import XCTest
+@testable import PokerHelper
+
+class PokerHelperTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
